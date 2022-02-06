ec30cdfa5d542ffb358a97b8f90e4a6c91bf45c2
wargunnerguy <reimovellemaa@gmail.com>
2022-02-06 16:42:00 +0100

    add fb chat plugin

--- plugins/vue-fb-customer-chat.js
+++ plugins/vue-fb-customer-chat.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import VueFbCustomerChat from 'vue-fb-customer-chat'
 
-Vue.use(VueFbCustomerChat, {
-  page_id: 2298596397103477, //  change 'null' to your Facebook Page ID,
-  theme_color: '#1bac91', // theme color in HEX
-  locale: 'et_EE', // default 'en_US'
-})
+export default function (context) {
+  const locale = context.app.i18n.locale;
+  const iso = context.app.i18n.locales.find(l => l.code === locale).iso;
+
+  Vue.use(VueFbCustomerChat, {
+    page_id: 109635857069303, //  change 'null' to your Facebook Page ID,
+    theme_color: '#1bac91', // theme color in HEX
+    locale: iso.replace('-', '_'), // default 'en_US'
+  })
+}
